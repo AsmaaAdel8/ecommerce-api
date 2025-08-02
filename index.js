c3ecd318fd690bc9e3070ae4265cc39cdab44b1f
Asmaa Adel <146136192+AsmaaAdel8@users.noreply.github.com>
2025-08-02 03:38:40 +0300

    final edit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createServer, Model } from 'json-server';
+import { createServer } from 'json-server';
 import users from './users.json';
 import products from './products.json';
 
@@ -9,6 +9,6 @@ server.db = {
 };
 
 server.use(jsonServer.router(server.db));
-server.listen(3000, () => {
-  console.log('JSON Server is running on port 3000');
+server.listen(8080||process.env.PORT, () => {
+  console.log('JSON Server is running on port 8080');
 });
