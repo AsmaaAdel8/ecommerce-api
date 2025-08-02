c504effd0876d02cefd06843ccca8d5f5cf05789
Asmaa Adel <146136192+AsmaaAdel8@users.noreply.github.com>
2025-08-02 03:54:22 +0300

    push it again

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-import { createServer } from 'json-server';
-import users from './users.json';
-import products from './products.json';
+import { createServer , router , middlewares} from "json-server";
+import users from "./users.json";
+import products from "./products.json";
 
 const server = createServer();
+const PORT = process.env.PORT || 8080;
 server.db = {
   users: users.users,
   products: products.products,
 };
 
-server.use(jsonServer.router(server.db));
-server.listen(8080||process.env.PORT, () => {
-  console.log('JSON Server is running on port 8080');
+server.use(middlewares);
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const jsonRouter = router(server.db);
+server.use(jsonRouter);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
